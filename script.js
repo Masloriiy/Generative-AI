cbcae31df77180d8339a3caa8fd5d33a3712cb98
Masloriiy <150061572+Masloriiy@users.noreply.github.com>
2023-11-09 19:17:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleAccordion(){
     this.nextElementSibling.classList.toggle('active');
 }
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors){
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
