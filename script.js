61abd36dadd3cd8bc2705c77130e264c6caaee7a
Masloriiy <150061572+Masloriiy@users.noreply.github.com>
2023-11-18 18:53:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,74 @@ for (let anchor of anchors){
         })
     })
 }
+
+
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    scrolling: false,
+    show() {
+        if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+            this.el.classList.remove('btn-up_hide');
+            this.el.classList.add('btn-up_hiding');
+            window.setTimeout(() => {
+                this.el.classList.remove('btn-up_hiding');
+            }, 300);
+        }
+    },
+    hide() {
+        if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+            this.el.classList.add('btn-up_hiding');
+            window.setTimeout(() => {
+                this.el.classList.add('btn-up_hide');
+                this.el.classList.remove('btn-up_hiding');
+            }, 300);
+        }
+    },
+    addEventListener() {
+        // при прокрутке окна (window)
+        window.addEventListener('scroll', () => {
+            const scrollY = window.scrollY || document.documentElement.scrollTop;
+            if (this.scrolling && scrollY > 0) {
+                return;
+            }
+            this.scrolling = false;
+            // если пользователь прокрутил страницу более чем на 200px
+            if (scrollY > 400) {
+                // сделаем кнопку .btn-up видимой
+                this.show();
+            } else {
+                // иначе скроем кнопку .btn-up
+                this.hide();
+            }
+        });
+        // при нажатии на кнопку .btn-up
+        document.querySelector('.btn-up').onclick = () => {
+            this.scrolling = true;
+            this.hide();
+            // переместиться в верхнюю часть страницы
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
+}
+
+btnUp.addEventListener();
+
+
+VANTA.WAVES({
+    el: "#vantaall",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x359aa0,
+    waveSpeed: 1.00,
+    shininess: 0.00,
+    zoom: 0.70
+})
